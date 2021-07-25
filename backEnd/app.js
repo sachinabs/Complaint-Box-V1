3dd729ad0c3367063dbaa46c3f2578ffa587bf61
sachin <sachinabs.js@gmail.com>
2021-07-25 19:59:13 +0530

    testdata

--- backEnd/app.js
+++ backEnd/app.js
@@ -6,17 +6,46 @@ var cors = require('cors');
 
 const app = express();
 
+// -------------------------------
+// assist functions
 
+function makeId(length) {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() *
+      charactersLength));
+  }
+  return result;
+}
+
+function dateAndTime()
+{
+  var today = new Date();
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  var time = today.getHours()+':'+today.getMinutes();
+  let cdata = date +' - '+  time;
+  return cdata;
+}
+// -------------------------------
 app.get("/submit",(req,res)=>{
 
-res.send("testing");
-
+  let Title = req.query.complaintTitle;
+  let Text = req.query.complaintDiscretion;
+  let myData = {
+    'ComplaintTitle': Title,
+    'ComplaintText': Text,
+    'ComplaintId': makeId(6),
+    'ComplaintTime':dateAndTime()
+  }
+res.send(myData);
 });
 
-app.get("/show",cors(),(req,res) => {
+app.get("/trend",cors(),(req,res) => {
   res.json({msg: 'This is CORS-enabled for a Single Route'})
 });
 
 
-const port = 6000;
+const port = 9099;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
--- frontEnd/custom_JS/httpCall.js
+++ frontEnd/custom_JS/httpCall.js
@@ -1,4 +1,12 @@
 function trendAndListData()
 {
-    alert("im ready to code");
+    var http = new XMLHttpRequest();
+    http.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("data").innerHTML= this.response;
+      }
+
+    };
+    http.open("GET", "http://127.0.0.1:9099/trend", true);
+    http.send();
 }
